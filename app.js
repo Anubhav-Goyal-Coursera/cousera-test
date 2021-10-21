120a265c9774be1ef7367f431ac472e223c5aaa2
NikhilNair-1 <92203665+NikhilNair-1@users.noreply.github.com>
2021-10-21 21:26:20 +0530

    new

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
-(function(){
+'use strict';
+(function () {
 
-  'use strict'; //for local scope
-angular.module('myFirstApp',[]);
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController)
 
-.controller('MyFirstController',function(){
 
-});
-})
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  $scope.items = '';
+  $scope.mssg = 'Please enter data first';
+
+  $scope.checkText = function() {
+    if ($scope.items.length == 0) {
+      $scope.mssg = 'Please enter data first';
+      return;
+    }
+    else {
+      let listItems = $scope.items.split(',').filter(value => value.trim().length!=0);
+      if (listItems.length <= 3) {
+        $scope.mssg = 'Enjoy!';
+      }
+      else {
+        $scope.mssg = 'Too much!';
+      }
+    }
+  }
+}
+
+})();
